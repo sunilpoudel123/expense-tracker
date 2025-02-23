47e9515fb4682ae925067f9c7451e2e3dba05d2b
Sunil Poudel <sunil.poudel@miu.edu>
2025-02-23 08:14:40 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
