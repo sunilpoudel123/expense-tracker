769d4de5fb113e9a55406577cda3569a33512a01
Sunil Poudel <sunilpoudel11@gmail.com>
2025-03-12 21:59:26 -0500

    index html is added for expense for

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ let totalAmount = 0;
 const categorySelect = document.getElementById('category-select');
 const amountInput = document.getElementById('amount-input');
 const dateInput = document.getElementById('date-input');
+
+const addBtn = document.getElementById('add-btn');
+const expenseTableBody = document.getElementById('expense-table-body');
+const expenseAmountCell = document.getElementById('total-amount');
+
+addBtn.addEventListener('click', function () {
+        const category = categorySelect.value;
+        const amount = Number(amountInput.addEventListener());
+        const date = dateInput.value;
+
+        if (category === '') {
+                alert('Please select a category');
+        }
+
+
+
+    }
+)
